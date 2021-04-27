c30a3b45adc8ba7e4b1a224e12b428ca51c6ab04
loonacuse <loonacuse@gmail.com>
2021-04-27 22:32:02 +0500

    Add Nez project template

---
+++ SteeringBehaviorsNez/GameRoot.cs
@@ -0,0 +1,16 @@
+﻿using Nez;
+using SteeringBehaviorsNez.Scenes;
+
+namespace SteeringBehaviorsNez
+{
+    public class GameRoot : Core
+    {
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            Scene = new SteeringBehaviorsScene();
+            DebugRenderEnabled = true;
+        }
+    }
+}
---
+++ SteeringBehaviorsNez/Scenes/SteeringBehaviorsScene.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using Nez.ImGuiTools;
+
+namespace SteeringBehaviorsNez.Scenes
+{
+    public class SteeringBehaviorsScene : Scene
+    {
+        public const int ScreenSpaceRenderLayer = 999;
+
+        private ImGuiManager _imGuiManager;
+
+        public SteeringBehaviorsScene()
+        {
+            AddRenderer(new ScreenSpaceRenderer(100, ScreenSpaceRenderLayer));
+            AddRenderer(new RenderLayerExcludeRenderer(0, ScreenSpaceRenderLayer));
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            _imGuiManager = new ImGuiManager();
+            Core.RegisterGlobalManager(_imGuiManager);
+
+            ClearColor = Color.CornflowerBlue;
+
+            SetDesignResolution(1360, 768, SceneResolutionPolicy.ShowAllPixelPerfect);
+            Screen.SetSize(1920, 1080);
+
+            
+        }
+    }
+}
